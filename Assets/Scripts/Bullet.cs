2b0ee49c9cd46e8f5675cc13af9725313cd9ed4f
KirKirKsd <sapetp54@gmail.com>
2023-02-21 17:54:31 +0300

    @

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed = 10f;
+
+    private void Update() {
+        rb.velocity = transform.up * speed;
+    }
+
+}
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour {
 
+    public GameObject bullet;
+    public Transform shootPoint;
+
     private void FixedUpdate() {
         transform.rotation = Quaternion.Euler(1f, 1f, Player.gunAngle);
         transform.localScale = new Vector3(Player.mirror, Player.mirror, 1f);
+
+        if (Input.GetMouseButton(0)) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        Instantiate(bullet, shootPoint.position, Quaternion.Euler(1f, 1f, Player.gunAngle - 90f));
     }
 
 }
